b05a31e23d7f2826840b920d153b7883dcaae733
Thomas Hessel <thomas.hessel@telekom.de>
2021-03-06 22:28:06 +0100

    Added models

--- Samples/wieruch/src/index.js
+++ Samples/wieruch/src/index.js
@@ -2,7 +2,9 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import { v4 as uuidv4 } from 'uuid';
- 
+
+import models from './models';
+
 const app = express();
 
 app.use(cors());
@@ -10,56 +12,35 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-  req.me = users[1];
+  req.context = {
+    models,
+    me: models.users[1]
+  };
   next();
 });
-
-let users = {
-  1: {
-    id: '1',
-    username: 'Robin Wieruch',
-  },
-  2: {
-    id: '2',
-    username: 'Dave Davids',
-  },
-};
-   
-let messages = {
-  1: {
-    id: '1',
-    text: 'Hello World',
-    userId: '1',
-  },
-  2: {
-    id: '2',
-    text: 'By World',
-    userId: '2',
-  },
-};
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
 app.get('/users', (req, res) => {
-  return res.send(Object.values(users));
+  return res.send(Object.values(req.context.models.users));
 });
    
 app.get('/users/:userId', (req, res) => {
-  return res.send(users[req.params.userId]);
+  return res.send(req.context.models.users[req.params.userId]);
 });
 
 app.get('/messages', (req, res) => {
-  return res.send(Object.values(messages));
+  return res.send(Object.values(req.context.models.messages));
 });
    
 app.get('/messages/:messageId', (req, res) => {
-  return res.send(messages[req.params.messageId]);
+  return res.send(req.context.models.messages[req.params.messageId]);
 });
 
 app.get('/session', (req, res) => {
-  return res.send(users[req.me.id]);
+  return res.send(req.context.models.users[req.me.id]);
 });
 
 app.post('/users', (req, res) => {
@@ -74,7 +55,7 @@ app.post('/messages', (req, res) => {
     userId: req.me.id
   };
  
-  messages[id] = message;
+  req.context.models.messages[id] = message;
  
   return res.send(message);
 });
@@ -96,7 +77,7 @@ app.delete('/messages/:messageId', (req, res) => {
     [req.params.messageId]: message,
     ...otherMessages
   } = messages;
-  messages = otherMessages;
+  req.context.models.messages = otherMessages;
  
   return res.send(message);
 });
