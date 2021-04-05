32e9ac3d0818c3a775f4fead61452abebf7236fc
Thomas Hessel <thomas.hessel@telekom.de>
2021-04-05 18:34:16 +0200

    Working example with Postgresql + Sequelize

--- Samples/wieruch/src/index.js
+++ Samples/wieruch/src/index.js
@@ -2,32 +2,44 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 
-import models from './models';
+import models, { sequelize } from './models';
 import routes from './routes';
 
 const app = express();
 
+// * Application-Level Middleware * //
+
+// Third-Party Middleware
+
 app.use(cors());
+
+// Built-In Middleware
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Custom Middleware
+
 app.use((req, res, next) => {
-  req.context = {
+    req.context = {
       models,
-      me: models.users[1]
-  };
-  next();
-});
+      me: models.users[1],
+    };
+    next();
+  });
+
+// * Routes * //
 
 app.use('/session', routes.session);
 app.use('/users', routes.user);
 app.use('/messages', routes.message);
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+// * Start * //
 
-app.listen(process.env.PORT, () => {
-    console.log(`Example app listening on port ${process.env.PORT}!`)
+const eraseDatabaseOnSync = true;
+
+sequelize.sync({ force: eraseDatabaseOnSync }).then(async () => {
+  app.listen(process.env.PORT, () =>
+    console.log(`Example app listening on port ${process.env.PORT}!`),
+  );
 });
